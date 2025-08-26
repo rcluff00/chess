70e75c9faf3b461d88cb751d3e040a462b99c92e
Ryan Cluff <ryan@qprosoftware.com>
2025-08-26 17:24:21 -0600

    tailwind init

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import Header from './modules/Header'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import Header from "./modules/Header";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <main className='max-w-2xl p-2 bg-slate-800 mx-auto rounded'>
+    <main className="mx-auto max-w-2xl rounded bg-slate-800 p-2">
       <Header logo={reactLogo} />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/modules/Header.jsx
+++ src/modules/Header.jsx
@@ -1,11 +1,8 @@
 export default function Header({ logo }) {
   return (
-    <header className="flex p-2 gap-2 items-center">
-      <img
-        src={logo}
-        alt="logo"
-      />
+    <header className="flex items-center gap-2 p-2">
+      <img src={logo} alt="logo" />
       <h1>Chess</h1>
     </header>
-  )
+  );
 }
