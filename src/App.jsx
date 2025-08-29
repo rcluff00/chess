455ff9dfd1910e697e4c1d14646d43579be6f381
Ryan Cluff <ryan@qprosoftware.com>
2025-08-29 17:10:02 -0600

    app has most the state

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,69 @@ import { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import Header from "./modules/Header"
 import Board from "./modules/Board"
+import { INITIAL_BOARD } from "./utils/InitalBoard"
+import { getAllowedMoves } from "./utils/AllowedMoves"
 
 import "./App.css"
 
 function App() {
   const [turns, setTurns] = useState([])
+  const [board, setBoard] = useState(INITIAL_BOARD)
+  const [selectedCoord, setSelectedCoord] = useState(null)
+
+  const allowedMoves = selectedCoord
+    ? getAllowedMoves(selectedCoord, board)
+    : []
+
+  function movePiece(prevCoord, targetCoord) {
+    const piece = board[prevCoord.row][prevCoord.col]
+    board[prevCoord.row][prevCoord.col] = null // remove piece
+    board[targetCoord.row][targetCoord.col] = piece // replace piece
+
+    setBoard(board)
+    setSelectedCoord(null)
+  }
+
+  function handleSquareClick(coord) {
+    const { row, col } = coord
+
+    // deselect same square
+    if (
+      selectedCoord &&
+      row === selectedCoord.row &&
+      col === selectedCoord.col
+    ) {
+      setSelectedCoord(null)
+    }
+
+    // move to valid square
+    else if (
+      allowedMoves &&
+      allowedMoves.some((m) => m.row === row && m.col === col)
+    ) {
+      movePiece(selectedCoord, coord)
+      setTurns((prevTurns) => {
+        const updatedTurns = [board, ...prevTurns]
+
+        return updatedTurns
+      })
+    }
+
+    // select new piece
+    else {
+      setSelectedCoord(coord)
+    }
+  }
 
   return (
     <main className="mx-auto w-full max-w-xl rounded bg-slate-800 p-2">
       <Header logo={reactLogo} />
-      <Board />
+      <Board
+        board={board}
+        onSquareClick={handleSquareClick}
+        allowedMoves={allowedMoves}
+        selectedCoord={selectedCoord}
+      />
     </main>
   )
 }
--- src/modules/Board.jsx
+++ src/modules/Board.jsx
@@ -1,55 +1,11 @@
-import { useState } from "react"
-import { getAllowedMoves } from "../utils/AllowedMoves"
-import { INITIAL_BOARD } from "../utils/InitalBoard"
 import Square from "./Square"
 
-export default function Board() {
-  const [board, setBoard] = useState(INITIAL_BOARD)
-  const [selectedCoord, setSelectedCoord] = useState(null)
-  const allowedMoves = selectedCoord
-    ? getAllowedMoves(selectedCoord, board)
-    : []
-
-  function movePiece(prevCoord, targetCoord) {
-    const piece = board[prevCoord.row][prevCoord.col]
-    board[prevCoord.row][prevCoord.col] = null // remove piece
-    board[targetCoord.row][targetCoord.col] = piece // replace piece
-
-    setBoard(board)
-    setSelectedCoord(null)
-    setTurns((prevTurns) => {
-      const updatedTurns = [board, ...prevTurns]
-
-      return updatedTurns
-    })
-  }
-
-  function handleSquareClick(coord) {
-    const { row, col } = coord
-
-    // deselect same square
-    if (
-      selectedCoord &&
-      row === selectedCoord.row &&
-      col === selectedCoord.col
-    ) {
-      setSelectedCoord(null)
-    }
-
-    // move to valid square
-    else if (
-      allowedMoves &&
-      allowedMoves.some((m) => m.row === row && m.col === col)
-    ) {
-      movePiece(selectedCoord, coord)
-    }
-
-    // select new piece
-    else {
-      setSelectedCoord(coord)
-    }
-  }
-
+export default function Board({
+  board,
+  onSquareClick,
+  allowedMoves,
+  selectedCoord,
+}) {
   return (
     <div className="grid grid-cols-8">
       {board.map((row, rowIndex) =>
@@ -69,7 +25,7 @@ export default function Board() {
               isAllowedMove={isAllowedMove}
               isSelected={isSelected}
               isDark={(rowIndex + colIndex) % 2 === 1}
-              onClick={() => handleSquareClick(coord)}
+              onClick={() => onSquareClick(coord)}
             />
           )
         }),
